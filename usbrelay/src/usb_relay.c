5c8cc265b4a801aff9501d7e79d34f2563971605
ArtBern <artem.bernadsky@gmail.com>
2017-10-17 18:08:47 +0300

    noextern

--- usbrelay/src/usb_relay.c
+++ usbrelay/src/usb_relay.c
@@ -59,9 +59,9 @@ int const PATH_OUT[]		= { 0xff000001, 0xff000002 };
 unsigned char const INIT_PACKET1[] = { 0x20, 0x00, 0x08, 0x01, 0x00, 0x00, 0x00, 0x00 };
 unsigned char const INIT_PACKET2[] = { 0x01, 0xd0, 0x08, 0x01, 0x00, 0x00, 0x00, 0x00 };
 
-    extern WMR *wmr_new( void );
-    extern int wmr_close( WMR * );
-    extern void wmr_print_state( unsigned int, int );
+#    extern WMR *wmr_new( void );
+#    extern int wmr_close( WMR * );
+#    extern void wmr_print_state( unsigned int, int );
 
 	
 WMR *wmr_new( void )
